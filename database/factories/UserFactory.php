348076e4d5ab55c14eb95b3a614a5d31706b00f2
João Pedro Volante <joaopedrovolante@gmail.com>
2024-11-01 09:13:04 -0300

    fix: login routes

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,13 +25,10 @@ class UserFactory extends Factory
     public function definition(): array
     {
         $codpes = $this->faker->unique()->servidor;
-        
-
         return [
             'codpes' => $codpes,
             'name' => Pessoa::nomeCompleto($codpes),
             'email' => Pessoa::email($codpes),
-            'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
         ];
@@ -42,7 +39,7 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
         ]);
     }
--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,4 @@ Route::get('/', function () {
     return view('welcome');
     Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
     Route::post('login', [LoginController::class, 'login']);
-    Route::get('/', [LivroController::class, 'index']);
 });
