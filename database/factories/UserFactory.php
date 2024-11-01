b459a34b81defdd9717fd942d5ae942853a3e365
João Pedro Volante <joaopedrovolante@gmail.com>
2024-11-01 08:54:37 -0300

    intall laravel/ui and config UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use Uspdev\Replicado\Pessoa;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -23,11 +24,15 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        $codpes = $this->faker->unique()->servidor;
+        
+
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'codpes' => $codpes,
+            'name' => Pessoa::nomeCompleto($codpes),
+            'email' => Pessoa::email($codpes),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
         ];
     }
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Faker\Factory as Faker;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        
+        $user = [
+            'codpes' => '123456',
+            'email' => 'qualquer@usp.br',
+            'name' => 'Fulano da Silva',
+            'is_admin' => true,
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi'
+        ];
+
+        User::create($user);
+
+        // Criando mais 10 usuários de teste usando a fábrica
+        User::factory(10)->create();
+    }
+}
